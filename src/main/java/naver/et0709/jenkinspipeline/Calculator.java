a1880d58b078d6ba994f7f36226d4b2709d2cdf8
king0jin <et0709@naver.com>
2024-11-08 14:45:39 +0900

    Commit & Push

--- src/main/java/naver/et0709/jenkinspipeline/Calculator.java
+++ src/main/java/naver/et0709/jenkinspipeline/Calculator.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
     public int sum(int a, int b){
-        System.out.println("Hello sum");
+        System.out.println("Hello Jenkins sum");
         return a + b;
     }
 }
