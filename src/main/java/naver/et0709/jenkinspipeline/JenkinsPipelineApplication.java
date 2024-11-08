4e3d816b11f579f102d4d2967dd5138c74728b4d
king0jin <et0709@naver.com>
2024-11-08 09:29:40 +0900

    JenkinsPipeline Project

---
+++ src/main/java/naver/et0709/jenkinspipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package naver.et0709.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/et0709/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
